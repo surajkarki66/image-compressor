679e9a23eb060c5f36cee00783ce6e72c57f747c
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-01 21:03:57 +0545

    Created electron main window

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("Electron");
+const { app, BrowserWindow } = require("electron");
+
+// Set platform
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isLinux = process.platform === "linux" ? true : false;
+
+let mainWindow;
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: "ImageCompressor",
+    width: 500,
+    height: 600,
+    icon: "./assets/icon/Icon_256x256.png",
+    resizable: isDev ? true : false,
+  });
+  mainWindow.loadFile(`./app/index.html`);
+  mainWindow.webContents.openDevTools();
+}
+
+app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
