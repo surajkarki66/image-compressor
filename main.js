d2445a825f497efd5fff9e37a1b599d6a1ba9822
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-02 20:02:47 +0545

    Added custom templates

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const isLinux = process.platform === "linux" ? true : false;
 const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
+let aboutWindow;
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: "ImageCompressor",
@@ -15,30 +16,73 @@ function createMainWindow() {
     height: 600,
     icon: "./assets/icon/Icon_256x256.png",
     resizable: isDev ? true : false,
+    backgroundColor: "white",
   });
   mainWindow.loadFile(`./app/index.html`);
-  mainWindow.webContents.openDevTools();
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: "About ImageCompressor",
+    width: 300,
+    height: 300,
+    icon: "./assets/icon/Icon_256x256.png",
+    resizable: false,
+    backgroundColor: "white",
+  });
+  aboutWindow.loadFile(`./app/about.html`);
 }
 
 app.on("ready", () => {
+  // Creating browser window
   createMainWindow();
+
+  // Adding main menu in BrowserWindow
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
+
+  // // Global shortcuts
+  // globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
+  // globalShortcut.register(isMac ? "Command+Alt+I" : "Ctrl+Shift+I", () =>
+  //   mainWindow.toggleDevTools()
+  // );
+
+  // Garbage Collection
   mainWindow.on("ready", () => (mainWindow = null));
 });
 
 // menu
 const menu = [
-  ...(isMac ? [{ role: "appMenu" }] : []),
-  {
-    label: "File",
-    submenu: [
-      {
-        label: "Quit",
-        click: () => app.quit(),
-      },
-    ],
-  },
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [{ label: "About", click: createAboutWindow }],
+        },
+      ]
+    : []),
+  { role: "fileMenu" },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [{ label: "About", click: createAboutWindow }],
+        },
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { role: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
 ];
 
 app.on("window-all-closed", () => {
